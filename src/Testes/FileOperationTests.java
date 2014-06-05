a7b26f3813356b38135c3e3671f2d492c1ba9765
Jose Moreira <ei12002@fe.up.pt>
2014-06-05 15:55:03 +0100

    Testes

---
+++ src/Testes/FileOperationTests.java
@@ -0,0 +1,44 @@
+package Testes;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import mainPackage.Cflow;
+
+import org.junit.Test;
+
+public class FileOperationTests {
+
+	@Test
+	public void testCopyDir() {
+		
+		String originalDir;
+		String destinationDir;
+		Scanner in = new Scanner(System.in);
+		 
+	      System.out.println("Enter source folder");
+	      originalDir = in.nextLine();
+	 
+	      System.out.println("Enter backup folder");
+	      destinationDir = in.nextLine();
+	      
+	      File source=new File(originalDir);
+	      File dest=new File(destinationDir);
+	      
+	      try {
+			Cflow.backupSourceCodeDir(source, dest);
+		} catch (IOException e) {
+			fail("error");
+		}
+	      
+	      assertTrue(true);
+	     
+		
+	}
+	
+	
+
+}
